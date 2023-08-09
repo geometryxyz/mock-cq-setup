62d0ac76d1cd1855c9cbf89503a52acea24c07f8
Andrija <akinovak@gmail.com>
2023-08-09 12:11:22 +0200

    test compute_lagrange_opening_commitments_at_zero

--- src/lagrange.rs
+++ src/lagrange.rs
@@ -53,7 +53,7 @@ pub fn lagrange_commitments<G: CurveGroup>(
 }
 
 
-pub fn lagrange_commitments_at_zero<G: CurveGroup>(
+pub fn lagrange_openings_commitments_at_zero<G: CurveGroup>(
     tau: G::ScalarField,
     n: usize,
     path: Option<&str>,
@@ -83,17 +83,17 @@ pub fn lagrange_commitments_at_zero<G: CurveGroup>(
     // X^{N-1} evaluated at tau
     let x_to_n_minus_one = tau.pow(&[n as u64 - 1]);
 
-    let mut lagrange_at_zero = vec![G::zero(); n as usize];
+    let mut lagrange_openings_at_zero = vec![G::zero(); n as usize];
 
     // w^(N - i)*L_i(tau) - 1/N * X^(N-1)
     #[cfg(not(feature = "parallel"))]
-    for (i, li) in lagrange_at_zero.iter_mut().enumerate() {
+    for (i, li) in lagrange_openings_at_zero.iter_mut().enumerate() {
         let w_inv_pow_i = w.pow(&[(n - i) as u64]);
         *li = gen.mul(w_inv_pow_i * lagrange_at_tau[i] - li_at_zero * x_to_n_minus_one);
     }
 
     #[cfg(feature = "parallel")]
-    parallelize(&mut lagrange_at_zero, |g, start| {
+    parallelize(&mut lagrange_openings_at_zero, |g, start| {
         for (idx, g) in g.iter_mut().enumerate() {
             let offset = start + idx;
             let w_inv_pow_i = w.pow(&[(n - offset) as u64]);
@@ -104,14 +104,14 @@ pub fn lagrange_commitments_at_zero<G: CurveGroup>(
     #[cfg(feature = "serialize")]
     {
         let path = path.expect("Path not provided");
-        let lagrange_at_zero_affine = G::normalize_batch(&lagrange_at_zero);
-        let data = serialize_points(&lagrange_at_zero_affine);
+        let lagrange_openings_at_zero_affine = G::normalize_batch(&lagrange_openings_at_zero);
+        let data = serialize_points(&lagrange_openings_at_zero_affine);
         write_points(path, &data);
         None
     }
 
     #[cfg(not(feature = "serialize"))]
-    Some(G::normalize_batch(&lagrange_at_zero))
+    Some(G::normalize_batch(&lagrange_openings_at_zero))
 }
 
 #[cfg(not(feature = "serialize"))]
@@ -119,15 +119,37 @@ pub fn lagrange_commitments_at_zero<G: CurveGroup>(
 mod test_lagrange {
     use std::ops::Mul;
 
-    use ark_bn254::{Fr, G1Affine};
+    use ark_bn254::{Fr, G1Affine, G1Projective};
     use ark_ec::{CurveGroup, Group};
-    use ark_ff::{batch_inversion, Field};
-    use ark_poly::{EvaluationDomain, GeneralEvaluationDomain};
+    use ark_ff::{batch_inversion, Field, FftField, Zero, One};
+    use ark_poly::{EvaluationDomain, GeneralEvaluationDomain, univariate::DensePolynomial, DenseUVPolynomial, Polynomial};
+
+    // given x coords construct Li polynomials
+    pub fn construct_lagrange_basis<F: FftField>(evaluation_domain: &[F]) -> Vec<DensePolynomial<F>> {
+        let mut bases = Vec::with_capacity(evaluation_domain.len());
+        for i in 0..evaluation_domain.len() {
+            let mut l_i = DensePolynomial::from_coefficients_slice(&[F::one()]);
+            let x_i = evaluation_domain[i];
+            for (j, _) in evaluation_domain.iter().enumerate() {
+                if j != i {
+                    let xi_minus_xj_inv = (x_i - evaluation_domain[j]).inverse().unwrap();
+                    l_i = &l_i
+                        * &DensePolynomial::from_coefficients_slice(&[
+                            -evaluation_domain[j] * xi_minus_xj_inv,
+                            xi_minus_xj_inv,
+                        ]);
+                }
+            }
+
+            bases.push(l_i);
+        }
+
+        bases
+    }
 
     // cargo test --features=parallel compute_lagrange_commitments
     #[test]
     fn compute_lagrange_commitments() {
-        use ark_bn254::{Fr, G1Projective};
         let k = 3;
         let n = 1 << k;
         let tau = Fr::from(100 as u64);
@@ -142,6 +164,36 @@ mod test_lagrange {
 
         let l_basis_coms = super::lagrange_commitments::<G1Projective>(tau, n as u64, None);
         assert_eq!(l_basis_coms.unwrap(), l_coms);
+    }
+
+    // cargo test --features=parallel compute_lagrange_opening_commitments_at_zero
+    #[test]
+    fn compute_lagrange_opening_commitments_at_zero() {
+        use ark_bn254::{Fr, G1Projective};
+        let k = 3;
+        let n = 1 << k;
+        let tau = Fr::from(100 as u64);
+
+        let domain = GeneralEvaluationDomain::<Fr>::new(n).unwrap();
+
+        let roots: Vec<_> = domain.elements().collect();
+        let l_basis = construct_lagrange_basis::<Fr>(&roots);
+
+        let l_at_zero = Fr::from(n as u64).inverse().unwrap();
+        let x_poly = DensePolynomial::from_coefficients_slice(&[Fr::zero(), Fr::one()]);
+
+        let quotients: Vec<DensePolynomial<_>> = l_basis.iter().map(|li| {
+            let mut li_x_minus_li_0 = li.clone();
+            li_x_minus_li_0[0] -= l_at_zero;
+
+            &li_x_minus_li_0 /&x_poly
+        }).collect();
+
+        let g = G1Projective::generator();
+        let q_commitments: Vec<G1Projective> = quotients.iter().map(|qi| g.mul(qi.evaluate(&tau))).collect();
+
+        let lagrange_openings_commitments = super::lagrange_openings_commitments_at_zero::<G1Projective>(tau, n, None);
+        assert_eq!(lagrange_openings_commitments.unwrap(), q_commitments);
     }
 
     #[test]
